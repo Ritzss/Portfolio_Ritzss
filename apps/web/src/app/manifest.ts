6131a68a9a36c55346799431b164ed66cffa8ab7
Ritzss <ritanshubabuta399@gmail.com>
2026-09-25 10:26:37 +0530

    feat: finalize portfolio

--- apps/web/src/app/manifest.ts
+++ apps/web/src/app/manifest.ts
@@ -10,7 +10,7 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     background_color: "#050505",
     theme_color: "#050505",
-    orientation: "portrait-primary",
+    orientation: "any",
     icons: [
       {
         src: "/icons/icon-192.png",
